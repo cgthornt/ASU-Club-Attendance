d0f8929e261a013d05b1e4c5f26444294646cfb8
Christopher Thornton <rmdirbin@gmail.com>
2013-09-26 11:42:02 -0700

    Secret token base!
    Todo: change in the server

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Clubtrack::Application.config.secret_token = '915a6e286edc9991154f760451cf285cc32160775f55bd7ff85101d14c4c0ecc2f88575d5491899f8bf95b5556c8f46d341376b3ab638ae04e070d5e6ffc37ab'
+Clubtrack::Application.config.secret_key_base = 'F6MbJRnk7Z0xnBmL5QVHF0IOcH2tILextr1SqqN8i57jk9Zgxtn3Gv2AQel0h'
